85a0d7154ef865ad7d3d9281b70cc96bd0c812ad
dimitar <dimitar.vukman@bluedropmedical.com>
2021-04-26 13:36:25 +0200

    Initial project.

---
+++ src/main/java/tech/dimitar/spring/statemachinedemo/StateMachineDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.dimitar.spring.statemachinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StateMachineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/dimitar/spring/statemachinedemo/StateMachineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimitar.spring.statemachinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
