58720409394c3d337a881528a5ac8090ec0fec4d
dimitar <dimitar.vukman@bluedropmedical.com>
2021-04-26 17:40:56 +0200

    States configuration.

--- src/main/java/tech/dimitar/spring/statemachinedemo/config/StateMachineConfig.java
+++ src/main/java/tech/dimitar/spring/statemachinedemo/config/StateMachineConfig.java
@@ -2,10 +2,20 @@ package tech.dimitar.spring.statemachinedemo.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigBuilder;
+import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.listener.StateMachineListener;
+import org.springframework.statemachine.listener.StateMachineListenerAdapter;
+import org.springframework.statemachine.state.State;
+import org.springframework.statemachine.transition.Transition;
+import tech.dimitar.spring.statemachinedemo.domain.Payment;
 import tech.dimitar.spring.statemachinedemo.domain.PaymentEvent;
 import tech.dimitar.spring.statemachinedemo.domain.PaymentState;
 
@@ -35,5 +45,20 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
                 .and()
                 .withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
+    }
+
+    @Override
+    public void configure(StateMachineConfigurationConfigurer<PaymentState, PaymentEvent> config) throws Exception {
+        final StateMachineListener<PaymentState, PaymentEvent> stateMachineListener = new StateMachineListenerAdapter<>() {
+            @Override
+            public void stateChanged(State<PaymentState, PaymentEvent> from, State<PaymentState, PaymentEvent> to) {
+                final String fromStateId = from != null ? from.getId().toString() : "null";
+                final String toStateId = to != null ? to.getId().toString() : "null";
+
+                log.info("State changed from : " + fromStateId + " to: " + toStateId);
+            }
+        };
+
+        config.withConfiguration().listener(stateMachineListener);
     }
 }
