3426c1a6f6e77e53ffdfd5e431046a15c5fe070c
dimitar <dimitar.vukman@bluedropmedical.com>
2021-04-26 16:29:31 +0200

    States configuration.

---
+++ src/main/java/tech/dimitar/spring/statemachinedemo/config/StateMachineConfig.java
@@ -0,0 +1,27 @@
+package tech.dimitar.spring.statemachinedemo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import tech.dimitar.spring.statemachinedemo.domain.PaymentEvent;
+import tech.dimitar.spring.statemachinedemo.domain.PaymentState;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+@Slf4j
+public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<PaymentState, PaymentEvent> states) throws Exception {
+        states.withStates()
+                .initial(PaymentState.NEW)
+                .states(EnumSet.allOf(PaymentState.class))
+                .end(PaymentState.AUTH)
+                .end(PaymentState.AUTH_ERROR)
+                .end(PaymentState.PRE_AUTH_ERROR);
+    }
+}
